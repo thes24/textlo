1eb1c301761b7ac80bbc28ff64694c97af1fc8ea
thes24 <seungwoo624@gmail.com>
2026-02-09 16:28:03 +0900

    style: prettier config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <div className='min-h-screen bg-gray-100'>
-        <h1 className='text-4xl font-bold text-center py-10'>
-          Textlo
-        </h1>
-        <p className='text-center text-gray-600'>
-          실시간 채팅 애플리케이션
-        </p>
+      <div className="flex min-h-screen bg-gray-100">
+        <h1 className="py-10 text-center text-4xl font-bold">Textlo</h1>
+        <p className="text-red text-center">실시간 채팅 애플리케이션</p>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
